9599aa01b8e1b29a032712056b0c857d38b521d0
Sundaram-Agnihotri <agnihotrisundaram8@gmail.com>
2024-03-02 15:03:45 +0530

    Starter Code

---
+++ src/main/java/com/embarkx/firstjobproject/FirstjobprojectApplication.java
@@ -0,0 +1,13 @@
+package com.embarkx.firstjobproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstjobprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstjobprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/embarkx/firstjobproject/FirstjobprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embarkx.firstjobproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstjobprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
